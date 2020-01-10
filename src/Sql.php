f40914e4d46d78f31ae2d237f791649a9b28d818
脆皮丶 <cuiqg@foxmail.com>
2020-01-11 00:49:08 +0800

    Update Sql.php

--- src/Sql.php
+++ src/Sql.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Str.php for helper.
  *               _
@@ -26,17 +27,18 @@ class Sql
      * @param integer $size
      * @return void
      */
-    public static function pager(mysqli $db, $sql, $page = 1, $size = 10) {
+    public static function pager(mysqli $db, $sql, $page = 1, $size = 10)
+    {
 
         $sql = preg_replace('/^select/i', 'select sql_calc_found_rows', $sql, 1);
-        $page = $page <= 0 ? 1 : intval( $page);
-        $size = $size <= 0 ? 10 : intval( $size);
+        $page = $page <= 0 ? 1 : intval($page);
+        $size = $size <= 0 ? 10 : intval($size);
 
         $offset = ($page - 1) * intval($size);
 
-        $sql .= " " ."limit {$offset},{$size}";
+        $sql .= " " . "limit {$offset},{$size}";
 
-        if(mysqli_connect_errno()) {
+        if (mysqli_connect_errno()) {
             throw new Exception(mysqli_connect_error(), -1);
         }
 
@@ -44,7 +46,7 @@ class Sql
 
         $rows = mysqli_query($db, $sql);
 
-        if(mysqli_errno($db)) {
+        if (mysqli_errno($db)) {
             throw new Exception(mysqli_error($db), -1);
         }
 
@@ -52,17 +54,17 @@ class Sql
 
         $total = mysqli_query($db, 'select found_rows()');
 
-        $total_count = current( mysqli_fetch_assoc($total));
+        $total_count = current(mysqli_fetch_assoc($total));
 
-        $total_pages = ceil( $total_count / $size);
+        $total_pages = ceil($total_count / $size);
 
-        $next = ($total_pages - $page) > 0 ? $page + 1 : $total_pages;
+        $next = ($total_pages - $page) > 0 ? $page + 1 : 0;
 
-        $prev = ($page - 1) > 0 ? $page - 1 : 1;
+        $prev = ($page - 1) > 0 ? $page - 1 : 0;
 
         $data = [];
 
-        while($row = mysqli_fetch_array($rows, MYSQLI_ASSOC)) {
+        while ($row = mysqli_fetch_array($rows, MYSQLI_ASSOC)) {
             $data[] = $row;
         }
 
