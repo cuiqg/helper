5f2a8b97382b98f3b13921b39274787754532c60
脆皮丶 <i@cuiqg.me>
2020-02-12 13:27:44 +0800

    Update Alioss.php

--- src/Alioss.php
+++ src/Alioss.php
@@ -3,8 +3,14 @@
 namespace Cuiqg\Helper;
 
 use OSS\OssClient as BaseOssClient;
+use OSS\Core\OssException;
 
 class Alioss extends BaseOssClient
+{
+    //
+}
+
+class AliossException extends OssException
 {
     //
 }
